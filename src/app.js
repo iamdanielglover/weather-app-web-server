8f19bf4d8d3e27bf3c1263b4537130bf9e75f2cf
github username <github email address>
2019-06-09 10:04:27 +0100

    added partials, organised views

--- src/app.js
+++ src/app.js
@@ -1,15 +1,18 @@
 const path = require('path')
 const express = require('express')
+const hbs = require('hbs')
 
 const app = express()
 
 // Define path fors Express config
 const publicDirectoryPath = path.join(__dirname, '../public')
-const viewsPath = path.join(__dirname, '../templates')
+const viewsPath = path.join(__dirname, '../templates/views')
+const partialsPath = path.join(__dirname, '../templates/partials')
 
 // Setup handlebars engine and views location
 app.set('view engine', 'hbs')
 app.set('views', viewsPath)
+hbs.registerPartials(partialsPath)
 
 // Setup static directory to serve
 app.use(express.static(publicDirectoryPath))
@@ -32,7 +35,8 @@ app.get('/about', (req, res) => {
 app.get('/help', (req, res) => {
     res.render('help', {
         title: 'Help Page',
-        message: 'Contact helpme@helpme.com if you have any issues.'
+        message: 'Contact helpme@helpme.com if you have any issues.',
+        name: 'Daniel Glover'
     })
 })
 
