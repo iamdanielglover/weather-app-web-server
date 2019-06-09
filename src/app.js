c05c963e6a3170d5fe3dc1ccd111db3f431b1c2b
github username <github email address>
2019-06-09 09:37:34 +0100

    basic templating in Express

--- src/app.js
+++ src/app.js
@@ -2,10 +2,39 @@ const path = require('path')
 const express = require('express')
 
 const app = express()
-const publicDirectoryPath = path.join(__dirname, '../public')
 
+// Define path fors Express config
+const publicDirectoryPath = path.join(__dirname, '../public')
+const viewsPath = path.join(__dirname, '../templates')
+
+// Setup handlebars engine and views location
 app.set('view engine', 'hbs')
+app.set('views', viewsPath)
+
+// Setup static directory to serve
 app.use(express.static(publicDirectoryPath))
+
+app.get('', (req, res) => {
+    res.render('index', {
+        title: 'Weather',
+        name: 'Daniel Glover'
+    })
+})
+
+app.get('/about', (req, res) => {
+    res.render('about', {
+        title: 'About Me',
+        image: '/img/snowmen.jpg',
+        name: 'Daniel Glover'
+    })
+})
+
+app.get('/help', (req, res) => {
+    res.render('help', {
+        title: 'Help Page',
+        message: 'Contact helpme@helpme.com if you have any issues.'
+    })
+})
 
 app.get('/weather', (req, res) => {
     res.send({
