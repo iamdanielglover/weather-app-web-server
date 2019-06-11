ac1c2501deb5f055d6732e65308da10c197d67a9
github username <github email address>
2019-06-11 17:32:54 +0100

    bringing fetch into the application

--- public/js/app.js
+++ public/js/app.js
@@ -1,1 +1,20 @@
 console.log('Client side Javascript file is loaded.')
+
+fetch('http://puzzle.mead.io/puzzle').then((response) => {
+    response.json().then((data) => {
+        console.log(data)
+    })
+})
+
+fetch('http://localhost:3000/weather?address=tenterden').then((response) => {
+    response.json().then((data) => {
+        console.log(data)
+    })
+})
+
+
+// fetch('http://localhost:3000/weather?address=tenterden').then((response) => {
+//     response.json().then((data) => {
+//         console.log(data)
+//     })
+// })
