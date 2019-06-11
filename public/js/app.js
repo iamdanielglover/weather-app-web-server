5c0805cb9850f0b1cf64ce9e0a61ea0b5931edf2
github username <github email address>
2019-06-11 20:12:28 +0100

    search form  built, frontend linked to backend

--- public/js/app.js
+++ public/js/app.js
@@ -1,20 +1,33 @@
 console.log('Client side Javascript file is loaded.')
 
-fetch('http://puzzle.mead.io/puzzle').then((response) => {
-    response.json().then((data) => {
-        console.log(data)
+const weatherForm = document.querySelector('form')
+const search = document.querySelector('input')
+const messageOne = document.querySelector('#message-1')
+const messageTwo = document.querySelector('#message-2')
+
+
+const fetchForecast = (address) => {
+    fetch(`http://localhost:3000/weather?address=${address}`).then((response) => {
+        response.json().then((data) => {
+            if (data.error) {
+                messageOne.textContent = data.error
+            } else {
+                messageOne.textContent = data.forecast
+                messageTwo.textContent = data.location
+            }
+        })
     })
+}
+
+// messageOne.textContent = ''
+
+weatherForm.addEventListener('submit',(e) => {
+    e.preventDefault()
+    
+    const location = search.value
+
+    messageOne.textContent = 'Loading...'
+    messageTwo.textContent = ''
+    // console.log(e.target[0].value)
+    fetchForecast(location)
 })
-
-fetch('http://localhost:3000/weather?address=tenterden').then((response) => {
-    response.json().then((data) => {
-        console.log(data)
-    })
-})
-
-
-// fetch('http://localhost:3000/weather?address=tenterden').then((response) => {
-//     response.json().then((data) => {
-//         console.log(data)
-//     })
-// })
